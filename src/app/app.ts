067157771ecffda5c7b2a74b11818f575731c7c9
Bishnupada Kamila <121252447+bishnu-kamila@users.noreply.github.com>
2025-07-15 10:44:11 +0530

    making new component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { sign } from 'crypto';
+import { Signup } from './signup/signup';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Signup],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected title = 'basic-app';
   name = 'Bishnupada Kamila';
-  hello(){
-    let x=20;
-    
-  }
+  
 }
